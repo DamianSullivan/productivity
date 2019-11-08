fa2d1cd8b380440f1b31c33ef06ac36620ff21cd
Damian Sullivan <djcsdjcs@gmail.com>
2019-11-07 20:43:08 -0600

    Switching to typescript for applet development.

--- workspace-timer@dsullivan/files/workspace-timer@dsullivan/applet.js
+++ workspace-timer@dsullivan/files/workspace-timer@dsullivan/applet.js
@@ -1,46 +1,21 @@
 const Applet = imports.ui.applet;
 const Lang = imports.lang;
 const Main = imports.ui.main;
-
-const UUID = "workspace-timer@dsullivan"
-
-let WorkspaceEventHandlerFactory; 
-if (typeof require !== 'undefined') {
-    WorkspaceEventHandlerFactory = require('./factory');
-} else {
-    const AppletDir = imports.ui.appletManager.applets[UUID]; 
-    WorkspaceEventHandlerFactory =
-        AppletDir.factory.WorkspaceEventHandlerFactory;
+const factory = require('./factory');
+function main(metadata, orientation, panelHeight, instanceId) {
+    var handler = factory.getWorkspaceEventHandler();
+    return new WorkspaceTimerApplet(metadata, orientation, panelHeight, instanceId, handler);
 }
-
-function main(metadata, orientation, panel_height, instanceId) {
-    factory = new WorkspaceEventHandlerFactory();
-    handler = factory.getWorkspaceEventHandler(); 
-    return new WorkspaceTimerApplet(
-        metadata, orientation, panel_height, instanceId, handler);
-}
-
-function WorkspaceTimerApplet(
-        metadata, orientation, panel_height, instanceId, handler) {
-    this._init(metadata, orientation, panel_height, instanceId, handler);
-}
-
-WorkspaceTimerApplet.prototype = {
-  __proto__: Applet.TextApplet.prototype,
-
-  _init: function(metadata, orientation, panel_height, instanceId, handler) {
-    Applet.TextApplet.prototype._init.call(
-        this, orientation, panel_height, instanceId);
-    this.metadata = metadata;
-    this.handler = handler;
-
-    try {
-        global.window_manager.connect('switch-workspace',
-            Lang.bind(this, this.handler.onSwitchWorkspace));
-        global.settings.connect('changed::workspace-name-overrides',
-            Lang.bind(this, this.handler.onOverrideWorkspaceName));
-    } catch(e) {
-        global.logError(e.toString());
+class WorkspaceTimerApplet extends Applet.TextIconApplet {
+    constructor(metadata, orientation, panelHeight, instanceId, handler) {
+        super(orientation, panelHeight, instanceId);
+        this.handler = handler;
+        try {
+            global.window_manager.connect('switch-workspace', Lang.bind(this, this.handler.onSwitchWorkspace));
+            global.settings.connect('changed::workspace-name-overrides', Lang.bind(this, this.handler.onOverrideWorkspaceName));
+        }
+        catch (e) {
+            global.logError(e.toString());
+        }
     }
-  },
 }
--- workspace-timer@dsullivan/files/workspace-timer@dsullivan/factory.js
+++ workspace-timer@dsullivan/files/workspace-timer@dsullivan/factory.js
@@ -1,35 +1,4 @@
-const Applet = imports.ui.applet;
-
-// TODO(dsullivan): This needs to come from a central place.
-const UUID = "workspace-timer@dsullivan"
-
-let TimeularWorkspaceEventHandler; 
-if (typeof require !== 'undefined') {
-    TimeularWorkspaceEventHandler =
-        require('./timeular/TimeularWorkspaceEventHandler');
-} else {
-    const AppletDir = imports.ui.appletManager.applets[UUID]; 
-    TimeularWorkspaceEventHandler =
-        AppletDir.timeular.TimeularWorkspaceEventHandler;
+var timeular = require('./timeular');
+function getWorkspaceEventHandler() {
+    return new timeular.TimeularWorkspaceEventHandler();
 }
-
-// Interface for workspace event handlers.
-var WorkspaceEventHandler = {
-    onSwitchWorkspace: function() {
-        global.log("onSwitchWorkspace Unimplemented") },
-    onOverrideWorkspaceName: function() {
-        global.log("onOverrideWorkspaceName Unimplemented") },
-}
-
-function WorkspaceEventHandlerFactory() {
-    this._init();
-}
-
-WorkspaceEventHandlerFactory.prototype = {
-    _init: function() {},
-
-	getWorkspaceEventHandler: function() {
-        return new TimeularWorkspaceEventHandler
-            .TimeularWorkspaceEventHandler();
-	}
-};
