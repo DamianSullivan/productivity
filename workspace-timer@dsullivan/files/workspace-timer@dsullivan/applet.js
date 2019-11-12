5d14d610ef7a1e3ca6d5cb3175cb0957538a0b0f
Damian Sullivan <djcsdjcs@gmail.com>
2019-11-12 04:20:21 -0600

    Tying it all together.

--- workspace-timer@dsullivan/files/workspace-timer@dsullivan/applet.js
+++ workspace-timer@dsullivan/files/workspace-timer@dsullivan/applet.js
@@ -6,26 +6,39 @@ const PopupMenu = imports.ui.popupMenu;
 const UUID = "workspace-timer@dsullivan";
 
 function main(metadata, orientation, panel_height, instanceId) {
-    return new WorkspaceNameApplet(metadata, orientation, panel_height, instanceId);
+    let WorkspaceEventHandlerFactory;
+    if (typeof require !== 'undefined') {
+        WorkspaceEventHandlerFactory = require('./factory');
+    } else {
+        let manager = imports.ui.appletManager.applets[metadata.uuid];
+        TimerModule = manager.factory;
+    }
+
+    let handler =
+        new WorkspaceEventHandlerFactory.getWorkspaceEventHandler();
+
+    return new WorkspaceNameApplet(
+        metadata, orientation, panel_height, instanceId, handler);
 }
 
-function WorkspaceNameApplet(metadata, orientation, panel_height, instanceId) {
-    this._init(metadata, orientation, panel_height, instanceId);
+function WorkspaceNameApplet(
+        metadata, orientation, panel_height, instanceId, handler) {
+    this._init(metadata, orientation, panel_height, instanceId, handler);
 }
 
 WorkspaceNameApplet.prototype = {
     __proto__: Applet.TextApplet.prototype,
 
-    _init: function(metadata, orientation, panel_height, instanceId) {
+    _init: function(metadata, orientation, panel_height, instanceId, handler) {
         Applet.TextApplet.prototype._init.call(
             this, orientation, panel_height, instanceId);
-        this.metadata = metadata;
+        this.handler = handler;
 
         try {
             global.window_manager.connect('switch-workspace',
-                Lang.bind(this, this.updateLabel));
+                Lang.bind(this, this.onSwitchWorkspace));
             global.settings.connect('changed::workspace-name-overrides',
-                Lang.bind(this, this.updateLabel));
+                Lang.bind(this, this.onSwitchWorkspace));
             this.updateLabel();
 
             // Set up the menu.
@@ -33,50 +46,15 @@ WorkspaceNameApplet.prototype = {
             this.menuManager = new PopupMenu.PopupMenuManager(this);
             this.menuManager.addMenu(this.menu);
         } catch(e) {
-            this.logError(
-                this.uuid + " Main Applet Exception: " + e.toString());
+            global.logError(e.toString());
         }
     },
 
-    on_applet_clicked: function(event) {
-        this.updateMenu();
-        this.menu.toggle();
-    },
-
-    log: function(message) {
-        global.log('[' + this.metadata.uuid + '] ' + message);
-    },
-
-    logError: function(message) {
-        global.logError('[' + this.metadata.uuid + '] ' + message);
-    },
-
-    updateLabel: function() {
-        this.log('Updating label');
+    onSwitchWorkspace: function() {
         let activeWorkspace = global.screen.get_active_workspace();
         let name = Main.getWorkspaceName(activeWorkspace.index());
         this.set_applet_label(name);
-    },
-
-    updateMenu: function() {
-        this.menu.removeAll();
-
-        for (let i = 0, workspaceCount = global.screen.n_workspaces;
-                i < workspaceCount; i++) {
-            let workspaceName = Main.getWorkspaceName(i);
-            let workspace = global.screen.get_workspace_by_index(i);
-            let menuItem = new PopupMenu.PopupMenuItem(workspaceName);
-
-            if (i === global.screen.get_active_workspace_index()) {
-                menuItem.actor.reactive = false;
-                menuItem.actor.can_focus = false;
-                menuItem.label.add_style_class_name('popup-subtitle-menu-item');
-            }
-
-            menuItem.connect('activate',
-                Lang.bind(this, function() { this.activateWorkspace(workspace); }));
-            this.menu.addMenuItem(menuItem);
-        }
+        this.handler.onSwitchWorkspace();
     },
 
     activateWorkspace: function(workspace) {
--- workspace-timer@dsullivan/files/workspace-timer@dsullivan/handler.js
+++ workspace-timer@dsullivan/files/workspace-timer@dsullivan/handler.js
@@ -1,4 +1,6 @@
+const Lang = imports.lang;
 const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
 
 const timeular = require('./timeular');
 
@@ -10,20 +12,39 @@ TimeularWorkspaceEventHandler.prototype = {
     _init: function() {
         // TODO(dsullivan): Replace this with configuration.
         this.api = new timeular.TimeularApi({
-            apiKey: "",
-            apiSecret: ""
+            apiKey: "NjQ0NDVfMTFmYTNjMTFkYTNjNGZiNzgyZjZkZGJiMWQ3MTNmZjE=",
+            apiSecret: "MzJjMThkZWZiNGIwNGUwMTg5NmEzYWNmYTIyYTljOTk=",
         });
         // TODO(dsullivan): Error handling.
         this.api.developerSignIn();
     },
 
-    onSwitchWorkspace() {
+    onSwitchWorkspace: function() {
         let activeWorkspace = global.screen.get_active_workspace();
-        let name = Main.getWorkspaceName(activeWorkspace.index());
-        global.log("Switching to workspace: " + name);
+        let activeWorkspaceName = Main.getWorkspaceName(activeWorkspace.index());
+        global.log("Switching to workspace: " + activeWorkspaceName);
+        // TODO(dsullivan): Seconds should come from a config.
+        Mainloop.timeout_add_seconds(
+            5, Lang.bind(this, function() {
+                this.startActivity(activeWorkspaceName);
+            }));
     },
 
-    onOverrideWorkspaceName() {
+    onOverrideWorkspaceName: function() {
         global.log("Overriding workspace: " + name);
     },
+
+    startActivity: function(workspace) {
+        let activeWorkspace = global.screen.get_active_workspace();
+        let activeWorkspaceName = Main.getWorkspaceName(activeWorkspace.index());
+
+        // Many workspace switches might have fired off, only start recording
+        // if the current workspace has been on screen for at least n seconds.
+        if (workspace != activeWorkspaceName) {
+            global.log("Workspace: " + workspace + " != Active: " + activeWorkspaceName);
+            return;
+        }
+
+        global.log("Starting to record: " + workspace);
+    }
 }
